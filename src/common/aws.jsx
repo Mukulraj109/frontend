7cf403de5b004d801f04dfaa6bd993492aadb8e8
Mukul <mukulraj.21ug2005@iiitranchi.ac.in>
2024-07-25 20:45:32 +0530

    changed env configuration

--- src/common/aws.jsx
+++ src/common/aws.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const uploadImage = async (file) => {
   try {
-    // Get the upload URL from the backend
-    const response = await axios.get('http://localhost:3000/generate-upload-url');
+    // Get the upload URL from the backend using the environment variable
+    const response = await axios.get(`${process.env.VITE_SERVER_DOMAIN}/generate-upload-url`);
     const uploadUrl = response.data.uploadUrl;
 
     if (!uploadUrl) {
